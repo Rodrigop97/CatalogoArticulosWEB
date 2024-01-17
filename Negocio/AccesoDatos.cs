638fe2cc783a91500f8b95749c5c5efb8c577165
nombre <rolo1486@gmail.com>
2024-01-17 09:41:52 -0300

    Inicio de proyecto

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Security.AccessControl;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        SqlConnection conexion;
+        SqlCommand comando;
+        SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            conexion.ConnectionString = "Server=.\\SQLEXPRESS; Database=CATALOGO_WEB_DB ;integrate security=true";
+            comando = new SqlCommand();
+        }
+
+        public void establecerConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+            
+        }
+        public void establecerParametors(string parametro, object valor)
+        {
+            comando.Parameters.AddWithValue(parametro, valor);
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
